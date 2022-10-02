11358fda4bc9a0cf269d23d4d42666e9f6a847ba
Jacob Truong <103521402+jacobmtruong@users.noreply.github.com>
2022-10-02 16:51:24 -0700

    Add files via upload

---
+++ practice/Daikichi_Routes/DaikichiRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.jacob.daikichiRoutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRoutesApplication.class, args);
+	}
+
+}
